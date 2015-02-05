0b4d98be93a3ef17161ede57d335533f75739194
Luke Ritchie <loritchie@gmail.com>
2015-02-05 10:44:14 +0000

    First Pass
    Button to turn on a light sequence

---
+++ Button_Press_NeoPixel.ino
@@ -0,0 +1,73 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(64, PIN, NEO_GRB + NEO_KHZ800);
+
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+int buttonState = 0;         // variable for reading the pushbutton status
+
+
+void setup() {
+  // put your setup code here, to run once:
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  
+  pinMode(buttonPin, INPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    // Some example procedures showing how to display to the pixels:
+  colorWipe(strip.Color(255, 0, 0), 50); // Red
+  colorWipe(strip.Color(0, 255, 0), 50); // Green
+  colorWipe(strip.Color(0, 0, 255), 50); // Blue
+  colorWipe(strip.Color(255, 20, 147), 50); // Pink
+  }
+  
+  
+  else {
+    // turn LED off:
+  clearStrip();
+  
+  } // ELSE
+  
+
+} // LOOP
+
+
+
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+
+void clearStrip() {
+  for( int i = 0; i<64; i++){
+    strip.setPixelColor(i, 0x000000); strip.show();
+  }
+}
+
+
